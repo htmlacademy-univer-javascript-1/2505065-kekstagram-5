1160e3eb1f9804d318e0ff63888d96b4f85ba517
Артём Пинькевич <Artem.Pinkevich@urfu.me>
2024-12-08 18:52:20 +0500

    5.16. Функции возвращаются

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+function checkMeetingTime(workStart, workEnd, meetingStart, meetingDuration) {
+  const workStartTime = parseTime(workStart);
+  const workEndTime = parseTime(workEnd);
+  const meetingStartTime = parseTime(meetingStart);
+  const meetingEndTime = meetingStartTime + meetingDuration;
+
+  return meetingStartTime >= workStartTime && meetingEndTime <= workEndTime;
+}
+
+function parseTime(timeStr) {
+  const [hours, minutes] = timeStr.split(':').map(Number);
+  return hours * 60 + minutes;
+}
+
+checkMeetingTime();
