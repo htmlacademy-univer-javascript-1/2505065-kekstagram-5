aabc5a5416a5200eb291eacd05f55f27f2457327
Артём Пинькевич <Artem.Pinkevich@urfu.me>
2024-12-09 22:03:05 +0500

    5.16. Функции возвращаются

--- js/data.js
+++ js/data.js
@@ -1,23 +1,9 @@
-import {
-  getRandomInteger,
-  getRandomArrayElement,
-  createIdGenerator,
-} from 'js/util.js';
-
 const PICTURE_COUNT = 25;
+const MIN_LIKES_COUNT = 15;
+const MAX_LIKES_COUNT = 200;
 const AVATAR_COUNT = 6;
-const LIKE_MIN_COUNT = 15;
-const LIKE_MAX_COUNT = 200;
-const COMMENTS_COUNT = 30;
-const NAMES = [
-  'Андрей',
-  'Валерий',
-  'Светлана',
-  'Александра',
-  'Анастасия',
-  'Вячеслав',
-];
-const MESSAGES = [
+const COMMENT_COUNT = 30;
+const COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -26,43 +12,81 @@ const MESSAGES = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 const DESCRIPTIONS = [
-  'Я на отдыхе с семьей',
-  'Люблю Сочи, езжу сюда каждый год #путешествия #summervibe',
-  'посмотрел новый сериал с женой #телевизор',
-  'Купила машину!!! #бойтесь',
-  'Ура, я переехал в Америку. Не знаю зачем, просто случайно вышло. #жизньнепредсказуема',
-  'опять не спал месяц',
-  'Я пересмотрел фильм "Драйв" с Райаном Гослингом.',
+  'Волшебный момент прощания солнца с днем. #Вечер #Пейзаж',
+  'Секретный рецепт, который покорил мое сердце! #Кулинария #Угощение',
+  'Лучезарные дни и свежий морской бриз — идеальное сочетание! #Отпуск #Море',
+  'Каждый цветок — это маленькое чудо природы. #Флора #Красота',
+  'На пути к захватывающим приключениям! #АктивныйОтдых #Путешествие',
+  'Утренний ритуал счастья с чашечкой кофе. #Утро #Напиток',
+  'Творчество как способ самовыражения. #Художество #ТворческийПроцесс',
+  'Любовь от пушистого друга радует сердце! #ДомашниеЖивотные #Радость',
 ];
+const NAMES = ['Олег', 'Семен', 'Аджелла', 'Люба', 'Глеб', 'Иван', 'Эдуард'];
+
+const getRandomInt = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.floor(Math.random() * (upper - lower + 1)) + lower;
+  return result;
+};
+
+const getRandomArrayElement = (items) =>
+  items[getRandomInt(0, items.length - 1)];
+
+const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
 
 const generateCommentId = createIdGenerator();
 
 const createMessage = () =>
-  Array.from({ length: getRandomInteger(1, 2) }, () =>
-    getRandomArrayElement(MESSAGES)
-  ).join(' ');
+  Array.from({ length: getRandomInt(1, 2) }, () =>
+    getRandomArrayElement(COMMENTS)
+  ).join('');
 
 const createComment = () => ({
-  id: generateCommentId,
-  avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
-  message: createMessage,
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInt(1, AVATAR_COUNT)}.svg`,
+  message: createMessage(),
   name: getRandomArrayElement(NAMES),
 });
 
 const createPicture = (index) => ({
   id: index,
   url: `photos/${index}.jpg`,
-  description: getRandomArrayElement(DESCRIPTIONS),
-  likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
+  descriptions: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInt(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
   comments: Array.from(
-    { length: getRandomInteger(0, COMMENTS_COUNT) },
+    { length: getRandomInt(0, COMMENT_COUNT) },
     createComment
   ),
 });
 
 const getPictures = () =>
-  Array.from({ length: PICTURE_COUNT }, (_, pictureIndex) =>
-    createPicture(pictureIndex + 1)
-  );
+  Array.from({ length: PICTURE_COUNT }, (_, index) => createPicture(index + 1));
 
-export { getPictures };
+getPictures();
+
+export {
+  PICTURE_COUNT,
+  MIN_LIKES_COUNT,
+  MAX_LIKES_COUNT,
+  AVATAR_COUNT,
+  COMMENT_COUNT,
+  COMMENTS,
+  DESCRIPTIONS,
+  NAMES,
+  getRandomInt,
+  getRandomArrayElement,
+  createIdGenerator,
+  generateCommentId,
+  createMessage,
+  createComment,
+  createPicture,
+  getPictures,
+};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-import { getPictures } from 'js/data.js';
-
-getPictures();
+/* eslint-disable no-unused-vars */
+import {
+  PICTURE_COUNT,
+  MIN_LIKES_COUNT,
+  MAX_LIKES_COUNT,
+  AVATAR_COUNT,
+  COMMENT_COUNT,
+  COMMENTS,
+  DESCRIPTIONS,
+  NAMES,
+  getRandomInt,
+  getRandomArrayElement,
+  createIdGenerator,
+  generateCommentId,
+  createMessage,
+  createComment,
+  createPicture,
+  getPictures,
+} from 'js/data.js';
+import { lenStrCheck } from 'js/functions.js';
